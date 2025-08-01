e7e7c53a0f5d4034dad95be7a25e919b951753d8
itsadhil <adhilquazi@gmail.com>
2025-08-01 10:01:27 +0530

    Completed Assignment

--- C2TC_CoreJavaProgram/src/dayeleven/v1/UserInterface.java
+++ C2TC_CoreJavaProgram/src/dayeleven/v1/UserInterface.java
@@ -9,7 +9,7 @@ public class UserInterface {
 			ArrayOperations obj= new ArrayOperations(new int[] {2,4,5,6});
 			obj.displayArray();
 			System.out.println("The element is: "+obj.getElement(2));
-			System.out.println("The element is: "+obj.getElement(10));
+			Systhttps://marketplace.eclipse.org/marketplace-client-intro?mpc_install=5725920em.out.println("The element is: "+obj.getElement(10));
 		}catch(ArrayIndexOutOfBoundsException ae) {
 			System.err.println("Error!..."+ ae.getMessage());
 		}
